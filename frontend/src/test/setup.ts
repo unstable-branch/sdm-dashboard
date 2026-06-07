cb7861731fb8dab1f81acb92bfcd0b3e118920c1
mrcanofcatfood <281064006+mrcanofcatfood@users.noreply.github.com>
2026-06-07 14:44:16 +1000

    fix: remove MSW from test setup (breaks fetch mocks in existing tests)

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,9 +1,1 @@
 import "@testing-library/jest-dom";
-import { setupServer } from "msw/node";
-import { getHandlers } from "../mocks/handlers";
-
-export const server = setupServer(...getHandlers());
-
-beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
